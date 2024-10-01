3618b4b1652f767cc8a1e3c8d66129822d52e97b
sergiorozada12 <sergiorozada12@gmail.com>
2024-10-01 09:38:23 +0200

    new exp

--- e1_single.py
+++ e1_single.py
@@ -25,13 +25,16 @@ nT = len(gs)
 gamma = 0.9
 
 num_experiments = 100
-E = 400
+E = 1000
 H = 100
 lr = 0.01
 eps = 0.1
 eps_decay = 1.0
 eps_min = 0.1
 k = 20
+
+n_upd = nT
+env_id = 0
 
 discretizer = Discretizer(
     min_points_states=[-1, -5],
@@ -90,13 +93,13 @@ def run_test_episode(Q, env_idx, H):
     with torch.no_grad():
         G = 0
         s, _ = envs[env_idx].reset()
-        s_idx = torch.tensor(tuple([env_idx]) + discretizer.get_state_index(s)).unsqueeze(0)
+        s_idx = torch.tensor(discretizer.get_state_index(s)).unsqueeze(0)
         for h in range(H):
             a_idx = Q(s_idx).argmax().item()
             a = discretizer.get_action_from_index(a_idx)
             a_idx = torch.tensor(a_idx).unsqueeze(0)
             sp, r, d, _, _ = envs[env_idx].step(a)
-            sp_idx = torch.tensor(tuple([env_idx]) + discretizer.get_state_index(sp)).unsqueeze(0)
+            sp_idx = torch.tensor(discretizer.get_state_index(sp)).unsqueeze(0)
 
             G += r
 
@@ -107,55 +110,54 @@ def run_test_episode(Q, env_idx, H):
             s_idx = sp_idx
     return G
 
-def run_experiment(exp_num, E, H, lr, eps, eps_decay, eps_min, k):
-    Q = PARAFAC(dims=[nT, nS, nS, nA], k=k, scale=0.01)
+def run_experiment(exp_num, E, H, lr, eps, eps_decay, eps_min, k, n_upd, env_id):
+    Gs = []
+    Q = PARAFAC(dims=[nS, nS, nA], k=k, scale=0.01)
     opt = torch.optim.Adamax(Q.parameters(), lr=lr)
-    Gs = [[] for _ in range(nT)]
-    
+    ds = 0
     for episode in range(E):
-        for env_idx, env in enumerate(envs):
-            s, _ = env.reset()
-            s_idx = torch.tensor(tuple([env_idx]) + discretizer.get_state_index(s)).unsqueeze(0)
-            for h in range(H):
-                a, a_idx = select_action(Q, s_idx, eps)
-                a_idx = torch.tensor(a_idx).unsqueeze(0)
-                sp, r, d, _, _ = env.step(a)
-                sp_idx = torch.tensor(tuple([env_idx]) + discretizer.get_state_index(sp)).unsqueeze(0)
+        s, _ = envs[env_id].reset()
+        s_idx = torch.tensor(discretizer.get_state_index(s)).unsqueeze(0)
+        for h in range(H):
+            a, a_idx = select_action(Q, s_idx, eps)
+            a_idx = torch.tensor(a_idx).unsqueeze(0)
+            sp, r, d, _, _ = envs[env_id].step(a)
+            sp_idx = torch.tensor(discretizer.get_state_index(sp)).unsqueeze(0)
 
-                s_idx_train = torch.tensor(discretizer.get_state_index(s)).unsqueeze(0)
-                sp_idx_train = torch.tensor(discretizer.get_state_index(sp)).unsqueeze(0)
-                update_model(s_idx_train, sp_idx_train, a_idx, r, Q, opt, tasks=torch.tensor([env_idx]))
+            for _ in range(n_upd):
+                update_model(s_idx, sp_idx, a_idx, r, Q, opt)
 
-                if d:
-                    break
+            if d:
+                ds += 1
+                break
 
-                s = sp
-                s_idx = sp_idx
-                eps = max(eps * eps_decay, eps_min)
+            s = sp
+            s_idx = sp_idx
+            eps = max(eps * eps_decay, eps_min)
 
-            if episode % 10 == 0:
-                G = run_test_episode(Q, env_idx, H)
-                Gs[env_idx].append(G)
+            if h % 10 == 0:
+                G = run_test_episode(Q, env_id, H)
+                Gs.append(G)
+        print(f"\rEpoch: {episode} - Return: {G}", end="", flush=True)
 
     return Gs
 
 def pad_Gs(Gs_list):
     max_length = max(len(g) for exp_Gs in Gs_list for g in exp_Gs)
-    padded_Gs = np.zeros((len(Gs_list), nT, max_length))
+    padded_Gs = np.zeros((len(Gs_list), max_length))
 
     for i, exp_Gs in enumerate(Gs_list):
-        for j, g in enumerate(exp_Gs):
-            padded_Gs[i, j, :len(g)] = g
+        padded_Gs[i, :len(exp_Gs)] = exp_Gs
 
     return padded_Gs
 
 if __name__ == '__main__':
     pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
-    results = pool.map(partial(run_experiment, E=E, H=H, lr=lr, eps=eps, eps_decay=eps_decay, eps_min=eps_min, k=k), range(num_experiments))
-
+    results = pool.map(partial(run_experiment, E=E, H=H, lr=lr, eps=eps, eps_decay=eps_decay, eps_min=eps_min, k=k, n_upd=n_upd, env_id=env_id), range(num_experiments))
+    
     pool.close()
     pool.join()
 
     # Pad and save the results
     padded_Gs = pad_Gs(results)
-    np.save("e2_multi.npy", padded_Gs)
+    np.save(f"e1_single_env{env_id}.npy", padded_Gs)
